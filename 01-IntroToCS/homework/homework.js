5a98eefef74e56ce75ee10020df3754c5c82106f
Peluchow <jesusaltamirano14@hotmail.com>
2022-07-04 18:13:50 -0500

    Homework1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,37 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let order='';
+  let multiplicacion;
+  let suma=0;
+  //change the position of the elements of string numero
+  for (let j=num.length-1;j>=0;j--) {      
+      order=order+num[j];        
+  }
 
+  for (let i=num.length -1 ; i >= 0 ; i--) {
+      multiplicacion=order[i]*Math.pow(2,i);
+      suma=suma+multiplicacion;
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let remainder;
+  let quotient=num;
+  let save='';
+  let resultado='';
+  
+  while(quotient>0){
+      remainder=quotient%2;
+      quotient=Math.floor(quotient/2);
+      save=save+remainder;
+  }
+  for (let i=save.length-1;i>=0;i--) {
+      resultado=resultado+save[i];
+  }
+  return resultado;
 }
 
 
